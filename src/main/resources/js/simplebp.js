b5510abac04b599557926676f48329971b7eacdc
atlassian_tutorial <documentation@atlassian.com>
2013-07-24 21:53:29 +0000

    simplebp.js edited online with Bitbucket

--- src/main/resources/js/simplebp.js
+++ src/main/resources/js/simplebp.js
@@ -1,1 +1,9 @@
- Confluence.Blueprint.setWizard('com.example.plugins.tutorial.confluence.simplebp:create-by-sample-template', function(wizard) {});
+Confluence.Blueprint.setWizard('com.example.plugins.tutorial.confluence.simplebp.simplebp:create-by-sample-template', function(wizard) {
+	 wizard.on('submit.page1Id', function(e, state) {	
+		 var vName = state.pageData.vName;
+		 if (!vName){
+			 alert('Please provide a name value.');
+			 return false;
+		 }
+	 });
+ });
