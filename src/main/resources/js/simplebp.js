258771e2b56f9472d34d797a01df90e525060523
Oliver Wehrens <oliver@wehrens.de>
2019-04-10 20:37:07 +0200

    First simple version

--- src/main/resources/js/simplebp.js
+++ src/main/resources/js/simplebp.js
@@ -1,8 +1,8 @@
 Confluence.Blueprint.setWizard('com.example.plugins.tutorial.confluence.simplebp.simplebp:create-by-sample-template', function(wizard) {
 	 wizard.on('submit.page1Id', function(e, state) {	
-		 var vName = state.pageData.vName;
+		 var vName = state.pageData.vThema;
 		 if (!vName){
-			 alert('Please provide a name value.');
+			 alert('Bitte Thema ausfüllen.');
 			 return false;
 		 }
 	 });
