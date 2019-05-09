10fa85f7000af27914b0576ca01298cbd7dd63b6
Oliver Wehrens <oliver@wehrens.de>
2019-05-09 15:02:55 +0200

    Setting the correct date.

--- src/main/java/net/wehrens/confluence/meeting/MyContextProvider.java
+++ src/main/java/net/wehrens/confluence/meeting/MyContextProvider.java
@@ -3,18 +3,31 @@ package net.wehrens.confluence.meeting;
 import com.atlassian.confluence.plugins.createcontent.api.contextproviders.AbstractBlueprintContextProvider;
 import com.atlassian.confluence.plugins.createcontent.api.contextproviders.BlueprintContext;
 
+import java.time.LocalDate;
+
 public class MyContextProvider extends AbstractBlueprintContextProvider {
 
     @Override
     protected BlueprintContext updateBlueprintContext(BlueprintContext blueprintContext) {
-        final String title = (String)blueprintContext.get("vThema");
+        final String title = (String) blueprintContext.get("vThema");
         blueprintContext.setTitle(title);
-
 
         String vVerantwortlich = (String) blueprintContext.get("vVerantwortlich");
         blueprintContext.put("vVerantwortlich", "<ac:link><ri:user ri:username=\"" + vVerantwortlich + "\"/></ac:link>");
-        blueprintContext.put("vDatum", "@currentDate");
+        LocalDate localDate = LocalDate.now();
+        String monthValue = getLeading0(localDate.getMonthValue());
+        String dayOfMonth = getLeading0(localDate.getDayOfMonth());
+        String d = localDate.getYear() + "-" + monthValue + "-" + dayOfMonth;
+        blueprintContext.put("vDatum", "<time datetime=\"" + d + "\" />");
 
         return blueprintContext;
+    }
+
+    public String getLeading0(int value) {
+        String result = "" + value;
+        if (value < 10) {
+            result = "0" + value;
+        }
+        return result;
     }
 }
